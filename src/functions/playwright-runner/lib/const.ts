c9a895e2027001fdc751b669184c3be9eef0ad57
ljourm <ljourm@gmail.com>
2025-11-09 23:25:47 +0900

    feat: スナップショットの同時実行数を4に変更

--- src/functions/playwright-runner/lib/const.ts
+++ src/functions/playwright-runner/lib/const.ts
@@ -5,7 +5,7 @@ export const VIEWPORT_HEIGHT = 2160;
 export const MAX_SNAPSHOT_HEIGHT = 10000;
 
 // スナップショット取得の同時実行数の上限
-export const CONCURRENCY_LIMIT = 1;
+export const CONCURRENCY_LIMIT = 4;
 
 // Lambda関数の最大ループ回数 (15分で終わらない場合、関数を何度も呼び出す)
 export const MAX_LAMBDA_LOOP_COUNT = 5;
